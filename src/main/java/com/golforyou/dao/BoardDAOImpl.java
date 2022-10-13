387ee27666c7bbd999c3e547f6fe4a01ae2bcd58
amazingkj <jiin724@gmail.com>
2022-10-13 17:09:15 +0900

    자유게시판 이진파일 업로드 기능 (김지인)

--- src/main/java/com/golforyou/dao/BoardDAOImpl.java
+++ src/main/java/com/golforyou/dao/BoardDAOImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.golforyou.vo.boardVO;
 
@@ -27,6 +28,49 @@ public class BoardDAOImpl implements BoardDAO {
 	public int getRowCount(boardVO b) {
 		return sqlSession.selectOne("board_count",b);
 	}//게시물 글 수
+
+
+	
+	@Override
+	public void replyboard(boardVO rb) {
+		this.sqlSession.insert("reply_in2",rb);
+	}
+
+	@Override
+	public void editboard(boardVO eb) {
+		this.sqlSession.update("board_update",eb);
+		
+	}
+
+	@Override
+	public void delboard(int b_no) {
+		this.sqlSession.delete("board_del",b_no);
+		
+	}
+
+	@Override
+	public boardVO getBoardCont(int b_no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateHit(int b_no) {
+		this.sqlSession.update("board_hit", b_no);
+		
+	}
+
+	@Override
+	public boardVO getBoard_cont(int b_no) {
+		return sqlSession.selectOne("board_co",b_no);
+		
+	}
+
+	@Override
+	public void updateLevel(boardVO rb) {
+		this.sqlSession.update("LevelUp",rb);
+		
+	}
 
 	
 
--- src/main/java/com/golforyou/service/BoardServiceImpl.java
+++ src/main/java/com/golforyou/service/BoardServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.golforyou.dao.BoardDAO;
 import com.golforyou.vo.boardVO;
@@ -27,6 +28,34 @@ public class BoardServiceImpl implements BoardService {
 	public int getRowCount(boardVO b) {
 		return boardDAO.getRowCount(b);
 	
+	}
+	@Transactional
+	@Override
+	public boardVO getBoardCont(int b_no) {
+		this.boardDAO.updateHit(b_no); //조회수 증가
+		return this.boardDAO.getBoard_cont(b_no);
+	}
+	@Override
+	public boardVO getBoardCont2(int b_no) {
+		return this.boardDAO.getBoard_cont(b_no);
+	}
+	
+	@Transactional
+	@Override
+	public void replyboard(boardVO rb) {
+		this.boardDAO.updateLevel(rb);
+		this.boardDAO.replyboard(rb);
+		
+	}
+	@Override
+	public void editboard(boardVO eb) {
+		this.boardDAO.editboard(eb);
+		
+	}
+	@Override
+	public void delboard(int b_no) {
+		this.boardDAO.delboard(b_no);	
+		
 	}
 
 
